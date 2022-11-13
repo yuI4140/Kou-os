41d6bb41863c7bb059b2c6cbb35fae91e2464b8f
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2022-11-13 20:06:56 +0100

    Update main.rs
    add  vga_buffer mod

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-use core::iter::Iterator;
+
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 /// This function is called on panic.
 #[panic_handler]
@@ -11,19 +13,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Watashi wa kanerudesu";
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    vga_buffer::print("hello bro");
     loop {}
 }
