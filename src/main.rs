32fd5f30affca1bb28db2be169e846b26b0252af
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2022-11-13 16:27:36 +0100

    Update main.rs
    now when you run it it will tell you 'Watashi wa kanerudesu'

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+// src/main.rs
+
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+use core::iter::Iterator;
 use core::panic::PanicInfo;
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
 
+static HELLO: &[u8] = b"Watashi wa kanerudesu";
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    loop {}
-}
+    let vga_buffer = 0xb8000 as *mut u8;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
