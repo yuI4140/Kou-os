0aaea34c96800e6e8afadb4bf530bc4eb6ea0c6b
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-01-15 18:33:10 +0100

    Summary
    Update projet

--- target/debug/deps/kernrust_toolchainel-23fb286dba75d6c6.d
+++
@@ -1,6 +0,0 @@
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\kernrust_toolchainel-23fb286dba75d6c6.rmeta: src\main.rs src\vga_buffer.rs
-
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\kernrust_toolchainel-23fb286dba75d6c6.d: src\main.rs src\vga_buffer.rs
-
-src\main.rs:
-src\vga_buffer.rs:
--- target/debug/deps/kernrust_toolchainel-af159d8fdcc40121.d
+++
@@ -1,6 +0,0 @@
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\kernrust_toolchainel-af159d8fdcc40121.rmeta: src\main.rs src\vga_buffer.rs
-
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\kernrust_toolchainel-af159d8fdcc40121.d: src\main.rs src\vga_buffer.rs
-
-src\main.rs:
-src\vga_buffer.rs:
