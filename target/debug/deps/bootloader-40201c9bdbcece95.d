214496d8ca542ddb4f0ae65f21f6d30fc2f99738
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-01-15 18:34:34 +0100

    remove Target

--- target/debug/deps/bootloader-40201c9bdbcece95.d
+++
@@ -1,7 +0,0 @@
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\bootloader-40201c9bdbcece95.rmeta: C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\lib.rs C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\bootinfo\mod.rs C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\bootinfo\memory_map.rs
-
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\bootloader-40201c9bdbcece95.d: C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\lib.rs C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\bootinfo\mod.rs C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\bootinfo\memory_map.rs
-
-C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\lib.rs:
-C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\bootinfo\mod.rs:
-C:\Users\ASUS\.cargo\registry\src\github.com-1ecc6299db9ec823\bootloader-0.9.23\src\bootinfo\memory_map.rs:
--- target/debug/deps/kernrust_toolchainel-964c25abf16ec374.d
+++
@@ -1,6 +0,0 @@
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\kernrust_toolchainel-964c25abf16ec374.rmeta: src\main.rs src\vga_buffer.rs
-
-d:\Greyber\GameDEV\repositories\Kou-os\target\debug\deps\kernrust_toolchainel-964c25abf16ec374.d: src\main.rs src\vga_buffer.rs
-
-src\main.rs:
-src\vga_buffer.rs:
